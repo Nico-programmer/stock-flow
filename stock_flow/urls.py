4757cba0941ce4508e221c64fbfbc50920126dfc
Nicolas Paulo Vega <nicolas.paulo.vega06@gmail.com>
2026-08-21 21:14:48 -0500

    feat: sidebar-design and core page

--- stock_flow/urls.py
+++ stock_flow/urls.py
@@ -17,9 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Static config
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Core
     path('', include('apps.accounts.urls')),
     path('home/', include('apps.core.urls')),
 ]
+
+if settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
