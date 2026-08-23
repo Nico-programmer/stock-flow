d9a3f1cb6c603ad9a185e5c22c6348c920ebc0a4
Nicolas Paulo Vega <nicolas.paulo.vega06@gmail.com>
2026-08-23 12:45:41 -0500

    feat: create templates users, design login, user and templates, new view 'userList_view'

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,4 +7,7 @@ from .views import *
 urlpatterns = [
     path('', login_view, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
+    # Users Wiews
+    path('user-list/', userList_view, name="users_list"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models import Q, Case, When, Value, IntegerField
 from django.shortcuts import render, redirect
-
 
 # Import forms
 from .forms import *
+
+# Import models
+from .models import *
+from apps.companies.models import *
 
 """ Login View """
 def login_view(request):
@@ -41,3 +46,53 @@ def login_view(request):
         form = LoginForm()
 
     return render(request, 'login.html', {'form': form})
+
+
+""" Users List Wiew """
+""" Users List View """
+def userList_view(request):
+    query = request.GET.get('q', '').strip()
+    role_filter = request.GET.get('role', '')
+    permission_filter = request.GET.get('permission', '')
+ 
+    users = (
+        CustomUser.objects
+        .filter(company=request.user.company)
+        .select_related('permissions')
+    )
+ 
+    if query:
+        users = users.filter(
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(username__icontains=query) |
+            Q(email__icontains=query)
+        )
+ 
+    if role_filter:
+        users = users.filter(role=role_filter)
+ 
+    if permission_filter:
+        # permission_filter llega como 'can_manage_inventory', 'can_manage_sales', etc.
+        users = users.filter(**{f'permissions__{permission_filter}': True})
+ 
+    # Orden fijo: Admin -> Encargado -> Empleado (no alfabético del value del choice)
+    users = users.annotate(
+        role_order=Case(
+            When(role=CustomUser.Role.ADMIN, then=Value(0)),
+            When(role=CustomUser.Role.MANAGER, then=Value(1)),
+            default=Value(2),
+            output_field=IntegerField(),
+        )
+    ).order_by('role_order', 'first_name', 'last_name')
+ 
+    paginator = Paginator(users, 10)
+    page_obj = paginator.get_page(request.GET.get('page'))
+ 
+    context = {
+        'page_obj': page_obj,
+        'query': query,
+        'role_filter': role_filter,
+        'permission_filter': permission_filter,
+    }
+    return render(request, "users/user_list.html", context)
